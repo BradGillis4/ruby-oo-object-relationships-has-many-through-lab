ad2651454182e88590bb733167382486aec156a8
BradGillis4 <bradgillis4@gmail.com>
2020-12-28 14:05:14 -0500

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,26 @@
-
+  
+class Artist
+    attr_reader :name
+    @@all = []
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+  
+    def songs
+      Song.all.select {|song| song.artist == self}
+    end
+  
+    def new_song(name, genre)
+      Song.new(name, self, genre)
+    end
+  
+    def genres
+      Song.all.map do |song| song.genre
+      end
+    end
+  
+    def self.all
+      @@all
+    end
+  end
